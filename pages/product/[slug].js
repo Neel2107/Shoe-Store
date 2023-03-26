c1b39635a409c9396a72128a16b2f527ca6c1f02
Neel <neelpatel805804@gmail.com>
2023-03-26 18:32:00 +0530

    Related Products with Carousel

--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -2,6 +2,7 @@ import React from "react";
 import { IoMdHeartEmpty } from "react-icons/io";
 import Wrapper from "@/components/Wrapper";
 import ProductDetailsCarousel from "@/components/ProductDetailsCarousel";
+import RelatedProducts from "@/components/RelatedProducts";
 
 const ProductDetails = () => {
   return (
@@ -32,23 +33,104 @@ const ProductDetails = () => {
             <div className="text-md font-medium text-black/[0.5] mb-20">
               {`(Also includes all applicable duties)`}
             </div>
-         
 
-          {/* PRODUCT SIZE RANGE START */}
-          <div className="mb-10">
-            {/* HEADING START */}
-            <div className="flex justify-between mb-2">
-              <div className="text-md font-semibold">Select Size</div>
-              <div className="text-md font-medium text-black/[0.5] cursor-pointer">
-                Select Guide
+            {/* PRODUCT SIZE RANGE START */}
+            <div className="mb-10">
+              {/* HEADING START */}
+              <div className="flex justify-between mb-2">
+                <div className="text-md font-semibold">Select Size</div>
+                <div className="text-md font-medium text-black/[0.5] cursor-pointer">
+                  Select Guide
+                </div>
               </div>
-            </div>
-            </div>  
-            {/* HEADING END */}
-          </div>
 
+              {/* HEADING END */}
+
+              {/* SIZE START */}
+              <div id="sizesGrid" className="grid grid-cols-3 gap-2">
+                <div className="border rounded-md text-center py-3 font-medium cursor-not-allowed bg-black/[0.1] opacity-50">
+                  UK 6
+                </div>
+                <div className="border rounded-md text-center py-3 font-medium hover:border-black cursor-pointer">
+                  UK 6.5
+                </div>
+                <div className="border rounded-md text-center py-3 font-medium hover:border-black cursor-pointer">
+                  UK 7
+                </div>
+                <div className="border rounded-md text-center py-3 font-medium hover:border-black cursor-pointer">
+                  UK 8
+                </div>
+                <div className="border rounded-md text-center py-3 font-medium hover:border-black cursor-pointer">
+                  UK 8.5
+                </div>
+                <div className="border rounded-md text-center py-3 font-medium hover:border-black cursor-pointer">
+                  UK 9
+                </div>
+                <div className="border rounded-md text-center py-3 font-medium cursor-not-allowed bg-black/[0.1] opacity-50">
+                  UK 9.5
+                </div>
+                <div className="border rounded-md text-center py-3 font-medium cursor-not-allowed bg-black/[0.1] opacity-50">
+                  UK 10
+                </div>
+                <div className="border rounded-md text-center py-3 font-medium cursor-not-allowed bg-black/[0.1] opacity-50">
+                  UK 11
+                </div>
+              </div>
+              {/* SIZE END */}
+              {/* SHOW ERROR START */}
+
+              <div className="text-red-600 mt-1">
+                Size selection is required
+              </div>
+
+              {/* SHOW ERROR END */}
+            </div>
+            {/* PRODUCT SIZE RANGE END */}
+
+            {/* ADD TO CART BUTTON START */}
+            <button className="w-full py-4 rounded-full bg-black text-white text-lg font-medium transition-transform active:scale-95 mb-3 hover:opacity-75">
+              Add to Cart
+            </button>
+            {/* ADD TO CART BUTTON END */}
+
+             {/* WHISHLIST BUTTON START */}
+             <button className="w-full py-4 rounded-full border border-black text-lg font-medium transition-transform active:scale-95 flex items-center justify-center gap-2 hover:opacity-75 mb-10">
+                            Whishlist
+                            <IoMdHeartEmpty size={20} />
+                        </button>
+                        {/* WHISHLIST BUTTON END */}
+
+
+<div>
+                            <div className="text-lg font-bold mb-5">
+                                Product Details
+                            </div>
+                            <div className="markdown text-md mb-5">
+                                {/* <ReactMarkdown>lorem34
+
+                                </ReactMarkdown> */}
+                           Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in 
+                            </div>
+                            <div className="markdown text-md mb-5">
+                                {/* <ReactMarkdown>lorem34
+
+                                </ReactMarkdown> */}
+                           Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo 
+                            </div>
+                        </div>
+
+
+
+
+
+          </div>
           {/* right column END  */}
         </div>
+
+
+<RelatedProducts/>
+
+
       </Wrapper>
     </div>
   );
